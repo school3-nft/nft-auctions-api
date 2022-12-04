001e8afc6e92aa751fe43cd09769845d3f55ec21
Kacper Klassa <kacperklassa@gmail.com>
2022-12-04 20:56:22 +0100

    add cors middleware

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.ledger.accounts import generate_ledger_account
 
 
 app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get('/')
 async def hello():
